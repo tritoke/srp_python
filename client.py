b0c9e11a8f14207a10a34d1ed2f9076ae35f7153
Sam <tritoke@protonmail.com>
2022-10-07 05:54:18 +0100

    Added to README and client.

--- client.py
+++ client.py
@@ -136,8 +136,8 @@ def main():
                 srp.register()
             else:
                 K = srp.negotiate()
-                print(f"agreed shared key: {K:X}")
-                print(srp.recv_encrypted(K))
+                print(f"agreed shared key: {K:0256X}")
+                print(f"Received encrypted message: {srp.recv_encrypted(K)}")
         except KeyError:
             if "success" not in srp.data:
                 raise
